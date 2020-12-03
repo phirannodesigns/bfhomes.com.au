83c63c50e4d6cb0ff76e5af023a335aed04e1ba3
Luke Bennett <hello@lukebennett.com.au>
2020-12-03 17:46:50 +1100

    Add MenuContextProvider

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,5 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+import * as React from 'react';
 import 'typeface-montserrat';
+import { MenuContextProvider } from './src/components/menu-context';
+
 import './src/styles/global.css';
+
+export function wrapRootElement({ element }) {
+  return <MenuContextProvider>{element}</MenuContextProvider>;
+}
