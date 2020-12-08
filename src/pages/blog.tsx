fcb8da995ef74c8f814f12a73590ec93b4049fcc
Luke Bennett <hello@lukebennett.com.au>
2020-12-08 19:51:07 +1100

    Add types

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { graphql } from "gatsby";
 import GatsbyImage from "gatsby-image";
 import { GatsbyImage as NewImage, getImage } from "gatsby-plugin-image";
+import GatsbyImage, { FluidObject } from "gatsby-image";
 import SanityBlockContent from "@sanity/block-content-to-react";
 import { HiArrowRight } from "react-icons/hi";
 
@@ -13,7 +14,7 @@ function LatestNewsPage({ data }) {
     <Layout>
       <SEO title="Latest News" />
       <Hero imageData={newHomes} />
-      <LatestBlogs posts={data.allSanityPost.nodes} />
+      <LatestBlogs nodes={data.allSanityPost.nodes} />
       <ContactSection />
     </Layout>
   );
@@ -23,7 +24,75 @@ function Hero({ imageData }) {
   return <NewImage image={imageData} alt="" />;
 }
 
-function LatestBlogs({ posts }) {
+interface LatestBlogsProps {
+  nodes: {
+    _publishedAt: string;
+    _rawBody: [];
+    categories: {
+      title: string;
+      id: string;
+    }[];
+    id: string;
+    imageAltText?: string;
+    mainImage: {
+      asset: {
+        fluid: FluidObject;
+      };
+    };
+    publishedAt: string;
+    slug: {
+      current: string;
+    };
+    title: string;
+  }[];
+}
+
+function LatestBlogs({ nodes }: LatestBlogsProps) {
+  // Filter posts from search input
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const [posts, setPosts] = React.useState(nodes);
+
+  // Filter search results from search results
+  React.useMemo(() => {
+    const filteredPosts = nodes.filter(
+      (node) =>
+        node.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        node.categories.find(
+          ({ title }) =>
+            title.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1
+        )
+    );
+    setPosts(filteredPosts);
+  }, [nodes, searchQuery]);
+
+  // Filter posts from select menu
+  const [categories, setCategories] = React.useState([]);
+
+  // State for filtering posts from select menu
+  const [filter, setFilter] = React.useState("All");
+
+  // Get unique categories
+  React.useMemo(() => {
+    const cat = [];
+    // Push all category titles to cat array
+    posts.map((post) => {
+      return post.categories.map((category) => cat.push(category.title));
+    });
+    // Filter duplicates and sort alphabetically
+    setCategories([...new Set(cat)].sort((a, b) => a.localeCompare(b)));
+  }, [posts]);
+
+  // Filter by tag
+  const filtered = React.useMemo(
+    () =>
+      filter === "All"
+        ? posts
+        : posts.filter((post) => {
+            return post.categories.some(({ title }) => title === filter);
+          }),
+    [filter, posts]
+  );
+
   return (
     <article className="text-white bg-brand-blue">
       <BGImageLeft>
@@ -36,21 +105,28 @@ function LatestBlogs({ posts }) {
             </div>
             <div className="grid gap-4 lg:grid-cols-2 text-brand-blue">
               <input
+                onChange={(e) => setSearchQuery(e.target.value)}
+                value={searchQuery}
                 type="text"
                 placeholder="Search"
                 className="font-medium transition duration-150 ease-in-out opacity-50 placeholder-uppercase placeholder-brand-blue focus:opacity-100"
               />
               <select
-                name=""
-                id=""
+                onChange={(e) => setFilter(e.target.value)}
+                value={filter}
                 className="font-medium uppercase transition duration-150 ease-in-out opacity-50 focus:opacity-100"
               >
-                <option value="Categories">Categories</option>
+                <option value="all">Categories</option>
+                {categories.map((category) => (
+                  <option key={category} value={category}>
+                    {category}
+                  </option>
+                ))}
               </select>
             </div>
           </div>
           <ul className="grid mt-10 gap-x-10 gap-y-16 lg:grid-cols-3">
-            {posts.map((post) => (
+            {filtered.map((post) => (
               <li key={post.id}>
                 <div className="relative h-0 aspect-w-4 aspect-h-3">
                   <div className="absolute inset-0 flex">
