035fad012eb65fef96addc87c3130570e3f658ba
Luke Bennett <hello@lukebennett.com.au>
2020-12-08 16:44:08 +1100

    Save layout progress for blog page

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,161 +1,19 @@
-import React, { useState, useMemo } from 'react';
-import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
-import GatsbyImage from 'gatsby-image';
-import { GatsbyImage as NewImage, getImage } from 'gatsby-plugin-image';
-import { HiArrowRight } from 'react-icons/hi';
+import * as React from "react";
+import { graphql } from "gatsby";
+import GatsbyImage from "gatsby-image";
+import { GatsbyImage as NewImage, getImage } from "gatsby-plugin-image";
+import SanityBlockContent from "@sanity/block-content-to-react";
+import { HiArrowRight } from "react-icons/hi";
 
-import { Layout, SEO, ContactSection, BGImageLeft } from '../components';
+import { Layout, SEO, ContactSection, BGImageLeft } from "../components";
 
-function LatestNewsPage({
-  data: {
-    allSanityPost: { nodes },
-  },
-  data,
-}) {
-  /**
-   * Filter posts from search input
-   */
-  const [searchQuery, setSearchQuery] = useState('');
-  const [posts, setPosts] = useState(nodes);
-
-  console.log(data);
+function LatestNewsPage({ data }) {
   const newHomes = getImage(data.newHomes);
-
-  // Filter search results from search results
-  useMemo(() => {
-    const filteredPosts = nodes.filter(
-      (node) =>
-        node.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-        node.categories.find(
-          ({ title }) =>
-            title.toLowerCase().indexOf(searchQuery.toLowerCase()) !== -1
-        )
-    );
-    setPosts(filteredPosts);
-  }, [nodes, searchQuery]);
-
-  /**
-   * Filter posts from select menu
-   */
-  const [categories, setCategories] = useState([]);
-
-  // State for filtering posts from select menu
-  const [filter, setFilter] = useState('All');
-
-  // Get unique categories
-  useMemo(() => {
-    const cat = [];
-    // Push all category titles to cat array
-    posts.map((post) => {
-      return post.categories.map((category) => cat.push(category.title));
-    });
-    // Filter duplicates and sort alphabetically
-    setCategories([...new Set(cat)].sort((a, b) => a.localeCompare(b)));
-  }, [posts]);
-
-  // Filter by tag
-  const filtered = useMemo(
-    () =>
-      filter === 'All'
-        ? posts
-        : posts.filter((post) => {
-            return post.categories.some(({ title }) => title === filter);
-          }),
-    [filter, posts]
-  );
-
   return (
     <Layout>
       <SEO title="Latest News" />
       <Hero imageData={newHomes} />
-      <article className="px-4 space-y-12 sm:px-6 lg:px-8 bg-brand-blue">
-        <BGImageLeft>
-          <div className="mx-auto text-center">
-            <h1 className="heading-1">Latest news.</h1>
-            <div className="grid max-w-2xl mx-auto mt-4 gap-x-6 gap-y-10 sm:grid-cols-2">
-              <div className="relative">
-                <label htmlFor="search-input">
-                  <span className="absolute inset-y-0 inline-flex items-center left-3">
-                    <svg
-                      className="w-5 h-5 text-gray-400"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                    </svg>
-                  </span>
-                  <input
-                    id="search-input"
-                    name="search-input"
-                    type="search"
-                    placeholder="Search"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
-                    className="w-full pl-10 rounded-full form-input"
-                  />
-                </label>
-              </div>
-              <div>
-                <select
-                  id="category-filter"
-                  name="category-filter"
-                  value={filter}
-                  onChange={(e) => setFilter(e.target.value)}
-                  className="w-full pl-4 rounded-full form-select"
-                >
-                  <option value="All">Select a category</option>
-                  {categories.map((category) => (
-                    <option key={category} value={category}>
-                      {category}
-                    </option>
-                  ))}
-                </select>
-              </div>
-            </div>
-          </div>
-          <div className="relative z-10 grid w-full max-w-screen-xl grid-cols-1 gap-12 px-4 py-20 mx-auto sm:grid-cols-2 mt-28 sm:mt-40 lg:grid lg:grid-cols-3 lg:gap-12 sm:px-6 lg:px-8">
-            {searchQuery !== '' && !posts.length ? (
-              <p className="text-center">
-                No results found! Please try a different search.
-              </p>
-            ) : (
-              <ul>
-                {filtered.map((post) => {
-                  return (
-                    <li key={post.id}>
-                      <GatsbyImage
-                        fluid={post.mainImage.asset.fluid}
-                        alt=""
-                        className="shadow-lg"
-                      />
-                      <h3 className="flex items-center mt-5 space-x-2 text-2xl font-bold uppercase text-brand-teal">
-                        <span>{post.title}</span>
-                        <HiArrowRight aria-hidden className="text-lg" />
-                      </h3>
-                      <div className="font-medium prose text-white">
-                        <p className="clamp-3">
-                          {post._rawBody.map((i) => `${i.children[0].text} `)}
-                        </p>
-                      </div>
-                      <div className="mt-1 font-medium text-brand-teal">
-                        <time>{post.publishedAt}</time>
-                        <span className="mx-3">|</span>
-                        {/* // TODO: Make this link work when posts have URL */}
-                        <a href="#">Share</a>
-                      </div>
-                    </li>
-                  );
-                })}
-              </ul>
-            )}
-          </div>
-        </BGImageLeft>
-      </article>
+      <LatestBlogs posts={data.allSanityPost.nodes} />
       <ContactSection />
     </Layout>
   );
@@ -165,16 +23,71 @@ function Hero({ imageData }) {
   return <NewImage image={imageData} alt="" />;
 }
 
-LatestNewsPage.propTypes = {
-  data: PropTypes.object.isRequired,
-};
+function LatestBlogs({ posts }) {
+  return (
+    <article className="text-white bg-brand-blue">
+      <BGImageLeft>
+        <div className="relative z-10 w-full max-w-screen-xl px-4 py-20 mx-auto sm:px-6 lg:px-8">
+          <div className="grid items-center gap-12 lg:grid-cols-2">
+            <div>
+              <h2 className="border-white heading-2">
+                Check Out Our Latest Blogs
+              </h2>
+            </div>
+            <div className="grid gap-4 lg:grid-cols-2 text-brand-blue">
+              <input
+                type="text"
+                placeholder="Search"
+                className="font-medium transition duration-150 ease-in-out opacity-50 placeholder-uppercase placeholder-brand-blue focus:opacity-100"
+              />
+              <select
+                name=""
+                id=""
+                className="font-medium uppercase transition duration-150 ease-in-out opacity-50 focus:opacity-100"
+              >
+                <option value="Categories">Categories</option>
+              </select>
+            </div>
+          </div>
+          <ul className="grid mt-10 gap-x-10 gap-y-16 lg:grid-cols-3">
+            {posts.map((post) => (
+              <li key={post.id}>
+                <div className="relative h-0 aspect-w-4 aspect-h-3">
+                  <div className="absolute inset-0 flex">
+                    <GatsbyImage
+                      fluid={post.mainImage.asset.fluid}
+                      alt=""
+                      className="flex-1 shadow-lg"
+                    />
+                  </div>
+                </div>
+                <h3 className="flex items-center mt-5 space-x-2 text-2xl font-bold uppercase text-brand-teal">
+                  <span>{post.title}</span>
+                  <HiArrowRight aria-hidden className="text-lg" />
+                </h3>
+                <div className="font-medium prose text-white clamp-3">
+                  <SanityBlockContent blocks={post._rawBody.slice(0, 1)} />
+                </div>
+                <div className="mt-1 font-medium text-brand-teal">
+                  <time dateTime={post._publishedAt}>{post.publishedAt}</time>
+                  <span className="mx-3">|</span>
+                  <a href="#">Share</a>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </BGImageLeft>
+    </article>
+  );
+}
 
 export const graphqlQuery = graphql`
   query MyQuery {
     allSanityPost(sort: { order: DESC, fields: publishedAt }) {
       nodes {
-        _createdAt(formatString: "MMMM DD, YYYY")
-        _rawBody(resolveReferences: { maxDepth: 50 })
+        _publishedAt: publishedAt
+        _rawBody
         categories {
           title
           id
