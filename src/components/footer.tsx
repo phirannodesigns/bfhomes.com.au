a2be2da3cb9edcdb7b22998467aa503d8ddd0586
Luke Bennett <hello@lukebennett.com.au>
2020-12-09 15:17:06 +1100

    Fix focus state

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
     <footer className="font-medium text-white bg-brand-blue">
       <div className="grid w-full max-w-screen-xl gap-12 px-4 pt-32 pb-12 mx-auto lg:grid-cols-3 sm:px-6 lg:px-8">
         <div className="w-full max-w-xs mx-auto transform -translate-y-12 lg:-translate-y-20">
-          <Link to="/">
+          <Link to="/" className="block">
             <span className="sr-only">{config.title}</span>
             <SiteLogo />
           </Link>
