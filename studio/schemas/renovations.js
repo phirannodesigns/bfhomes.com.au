be1c047642c37adae753efd3aff2c34ca7f48857
Luke Bennett <hello@lukebennett.com.au>
2021-02-08 12:22:44 +1100

    Update schema

--- studio/schemas/renovations.js
+++ studio/schemas/renovations.js
@@ -4,6 +4,28 @@ export default {
   type: 'document',
   fields: [
     {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      title: 'Hero image',
+      name: 'heroImage',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
       name: 'renovation',
       title: 'Renovation Areas',
       type: 'array',
