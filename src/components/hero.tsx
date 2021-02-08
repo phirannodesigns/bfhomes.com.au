e5368054c822df6fe7b45be184d7d24ec131a6d0
Luke Bennett <hello@lukebennett.com.au>
2021-02-08 12:22:56 +1100

    accept image source or children

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,18 @@
-import { GatsbyImage } from 'gatsby-plugin-image';
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
 import * as React from 'react';
 
-function Hero({ imageData }) {
+interface IHero {
+  children?: React.ReactNode;
+  imageData?: IGatsbyImageData;
+}
+
+function Hero({ children, imageData }: IHero) {
   return (
     <div className="relative aspect-w-16 aspect-h-9">
       <div className="absolute inset-0 flex">
-        <GatsbyImage image={imageData} alt="" className="flex-1" />
+        {children || (
+          <GatsbyImage image={imageData} alt="" className="flex-1" />
+        )}
       </div>
     </div>
   );
