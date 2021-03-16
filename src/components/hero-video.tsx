ea90e5164bb01d18a23dbbebad677dc4edc1ed4b
JD Manabat <manabat.johnder@gmail.com>
2021-03-16 08:28:50 +0800

    Mute hero YT video

--- src/components/hero-video.tsx
+++ src/components/hero-video.tsx
@@ -11,7 +11,7 @@ function HeroVideo({ YTVideoID, imageData }) {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           frameBorder={0}
           title="YouTube video player"
-          src={`https://www.youtube.com/embed/${YTVideoID}?autoplay=1&controls=0&disablekb=1&iv_load_policy=3&loop=1&modestbranding=1&playlist=${YTVideoID}&playsinline=1&rel=0`}
+          src={`https://www.youtube.com/embed/${YTVideoID}?autoplay=1&mute=1&controls=0&disablekb=1&iv_load_policy=3&loop=1&modestbranding=1&playlist=${YTVideoID}&playsinline=1&rel=0`}
           className="absolute inset-0 pointer-events-none"
         />
       </div>
